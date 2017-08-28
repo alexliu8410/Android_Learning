6cd10ced36c0fd4da4d5e9e458f31220b41122a4
Alex <liuale@amazon.com>
2017-08-28 16:44:11 -0700

    [Android Learning]This is my day1 of my own Android project

---
+++ MyYoutubeAppDemo/app/src/main/java/com/example/liuale/myyoutubeappdemo/MyCard.java
@@ -0,0 +1,46 @@
+package com.example.liuale.myyoutubeappdemo;
+
+/**
+ * Created by liuale on 8/28/17.
+ */
+
+public class MyCard
+{
+
+    String cardName;
+    int imageResourceId;
+    int isfav;
+    int isturned;
+
+    public int getIsturned() {
+        return isturned;
+    }
+
+    public void setIsturned(int isturned) {
+        this.isturned = isturned;
+    }
+
+    public int getIsfav() {
+        return isfav;
+    }
+
+    public void setIsfav(int isfav) {
+        this.isfav = isfav;
+    }
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public void setCardName(String cardName) {
+        this.cardName = cardName;
+    }
+
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+
+    public void setImageResourceId(int imageResourceId) {
+        this.imageResourceId = imageResourceId;
+    }
+}
