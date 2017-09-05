0d5be27402ece3ffd4c12cddd3945c20675dc109
Alex <liuale@amazon.com>
2017-09-05 11:49:13 -0700

    [Android Learning]This is the optimization of MyAdapter

--- MyYoutubeAppDemo/app/src/main/java/com/example/liuale/myyoutubeappdemo/MyAdapter.java
+++ MyYoutubeAppDemo/app/src/main/java/com/example/liuale/myyoutubeappdemo/MyAdapter.java
@@ -44,12 +44,71 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> impl
 
     @Override
     public void onBindViewHolder(final MyViewHolder holder, int position) {
-        holder.titleTextView.setText(mYoutubeVideoItemList.get(position).getVideoTitle());
-        holder.dateTextView.setText(mYoutubeVideoItemList.get(position).getReleaseDate());
+
+        final TextView titleTextView = holder.titleTextView;
+        final TextView dateTextView = holder.dateTextView;
+        final ImageView likeImageView = holder.likeImageView;
+        final ImageView shareImageView = holder.shareImageView;
+        final ImageView coverImageView = holder.coverImageView;
+
+        titleTextView.setText(mYoutubeVideoItemList.get(position).getVideoTitle());
+        dateTextView.setText(mYoutubeVideoItemList.get(position).getReleaseDate());
         holder.setVideoId(mYoutubeVideoItemList.get(position).getVideoId());
-        holder.likeImageView.setTag(R.drawable.ic_like);
-        holder.shareImageView.setTag(R.drawable.ic_share);
-        Picasso.with(MainActivity.getAppContext()).load(mYoutubeVideoItemList.get(position).getCoverImageUrl()).into(holder.coverImageView);
+        likeImageView.setTag(R.drawable.ic_like);
+        shareImageView.setTag(R.drawable.ic_share);
+
+        final Context context = MainActivity.getAppContext();
+
+        holder.coverImageView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.getAppContext(), YouTubePlayActivity.class);
+                intent.putExtra(INTENT_VIDEO_ID,holder.getVideoId());
+                MainActivity.getAppContext().startActivity(intent);
+            }
+        });
+
+
+        holder.likeImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int id = (int)likeImageView.getTag();
+
+                if( id == R.drawable.ic_like){
+
+                    likeImageView.setTag(R.drawable.ic_liked);
+                    likeImageView.setImageResource(R.drawable.ic_liked);
+                    Toast.makeText(MainActivity.getAppContext(),titleTextView.getText()+" added to favourites",Toast.LENGTH_SHORT).show();
+
+                }else{
+
+                    likeImageView.setTag(R.drawable.ic_like);
+                    likeImageView.setImageResource(R.drawable.ic_like);
+                    Toast.makeText(MainActivity.getAppContext(),titleTextView.getText()+" removed from favourites",Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+
+        shareImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Uri imageUri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE +
+                        "://" + context.getResources().getResourcePackageName(shareImageView.getId())
+                        + '/' + "drawable" + '/' + context.getResources().getResourceEntryName((int)shareImageView.getTag()));
+
+                Intent shareIntent = new Intent();
+                shareIntent.setAction(Intent.ACTION_SEND);
+                shareIntent.putExtra(Intent.EXTRA_STREAM,imageUri);
+                shareIntent.setType("image/jpeg");
+                context.startActivity(Intent.createChooser(shareIntent, context.getResources().getText(R.string.send_to)));
+            }
+        });
+
+        Picasso.with(MainActivity.getAppContext()).load(mYoutubeVideoItemList.get(position).getCoverImageUrl()).into(coverImageView);
     }
 
     @Override
@@ -65,16 +124,16 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> impl
         mRecyclerView.setAdapter(this);
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
 
-        private TextView titleTextView;
-        private TextView dateTextView;
-        private ImageView coverImageView;
-        private ImageView likeImageView;
-        private ImageView shareImageView;
-        private String videoId;
+        public TextView titleTextView;
+        public TextView dateTextView;
+        public ImageView coverImageView;
+        public ImageView likeImageView;
+        public ImageView shareImageView;
+        public String videoId;
 
-        private MyViewHolder(View v) {
+        public MyViewHolder(View v) {
             super(v);
             titleTextView = (TextView) v.findViewById(R.id.titleTextView);
             dateTextView = (TextView) v.findViewById(R.id.dataTextView);
@@ -82,56 +141,6 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> impl
             likeImageView = (ImageView) v.findViewById(R.id.likeImageView);
             shareImageView = (ImageView) v.findViewById(R.id.shareImageView);
 
-            final Context context = MainActivity.getAppContext();
-
-            coverImageView.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View view){
-                    Intent intent = new Intent(MainActivity.getAppContext(), YouTubePlayActivity.class);
-                    intent.putExtra(INTENT_VIDEO_ID,videoId);
-                    MainActivity.getAppContext().startActivity(intent);
-                }
-            });
-
-
-            likeImageView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    int id = (int)likeImageView.getTag();
-
-                    if( id == R.drawable.ic_like){
-
-                        likeImageView.setTag(R.drawable.ic_liked);
-                        likeImageView.setImageResource(R.drawable.ic_liked);
-                        Toast.makeText(MainActivity.getAppContext(),titleTextView.getText()+" added to favourites",Toast.LENGTH_SHORT).show();
-
-                    }else{
-
-                        likeImageView.setTag(R.drawable.ic_like);
-                        likeImageView.setImageResource(R.drawable.ic_like);
-                        Toast.makeText(MainActivity.getAppContext(),titleTextView.getText()+" removed from favourites",Toast.LENGTH_SHORT).show();
-
-                    }
-
-                }
-            });
-
-            shareImageView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    Uri imageUri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE +
-                            "://" + context.getResources().getResourcePackageName(shareImageView.getId())
-                            + '/' + "drawable" + '/' + context.getResources().getResourceEntryName((int)shareImageView.getTag()));
-
-                    Intent shareIntent = new Intent();
-                    shareIntent.setAction(Intent.ACTION_SEND);
-                    shareIntent.putExtra(Intent.EXTRA_STREAM,imageUri);
-                    shareIntent.setType("image/jpeg");
-                    context.startActivity(Intent.createChooser(shareIntent, context.getResources().getText(R.string.send_to)));
-                }
-            });
         }
 
         public void setVideoId(String Id){
